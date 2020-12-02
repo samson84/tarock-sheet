69516f19514fdbdfe5055c992ce83711d02e2506
Daniel Hatas <samson84@gmail.com>
2020-12-02 01:28:42 +0100

    Add an AppBar to the App

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import React from "react";
-import { Container, Paper, Typography as T } from "@material-ui/core";
+import { AppBar, Container, Paper, Typography as T } from "@material-ui/core";
 import { CgCardHearts as Icon } from "react-icons/cg";
 import TarockSheet from "./components/TarockSheet";
 
 
 function App() {
   return (
-    <Container>
+    <>
+      <AppBar position="static">
         <T variant="h2"><Icon color="red" /> Tarock Sheet</T>
+      </AppBar>
+      <Container>
         <TarockSheet />
-    </Container>
+      </Container>
+    </>
   );
 }
 
