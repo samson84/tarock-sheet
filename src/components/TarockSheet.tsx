cbf0cec99dc4ab058d8f974506908dee64c0aa45
Daniel Hatas <samson84@gmail.com>
2020-12-02 01:28:17 +0100

    add bid selector component to TarockSheet

--- src/components/TarockSheet.tsx
+++ src/components/TarockSheet.tsx
@@ -1,20 +1,18 @@
-import React from "react";
-import {Grid} from "@material-ui/core"
-import { BID_TYPE, getAllBids, Bid } from "../lib/bid";
-import { upperCaseToWords } from "../lib/util";
-import BidElement from "./Bid";
+import React, { useState } from "react";
+import { getAllBids, Bid } from "../lib/bid";
+import BidSelector from "./BidSelector";
 import sortBy from "lodash/fp/sortBy"
+import { Contract } from "../lib/contract";
 
 const allBids = sortBy((b: Bid) => b.type )(getAllBids())
 
-export default function() {
+export default () => {
+  const [contract, setContract] = useState<Contract|null>(null)
   return (
-    <Grid container spacing={1}>
-      {allBids.map(bid => (
-        <Grid item>
-          <BidElement bid={bid}/>
-        </Grid>
-      )) }
-    </Grid>
-  )
+    <>
+      <BidSelector bids={allBids} onAddContract={setContract}/>
+      <pre>{JSON.stringify(contract, null, 2)}</pre>
+    </>
+  
+    )
 }
