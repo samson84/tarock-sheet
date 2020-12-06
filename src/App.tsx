581d727031436d7fe3d0432802075c82960879dd
Daniel Hatas <samson84@gmail.com>
2020-12-06 23:41:11 +0100

    add some spacing to the main App

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
 import React from "react";
-import { AppBar, Container, Paper, Typography as T } from "@material-ui/core";
+import { AppBar, Container, makeStyles, Paper, Typography as T } from "@material-ui/core";
 import { CgCardHearts as Icon } from "react-icons/cg";
 import TarockSheet from "./components/TarockSheet";
 
+const useAppStyle = makeStyles((theme) => ({
+  container: {
+    padding: theme.spacing(4)
+  }
+}))
 
 function App() {
+  const classes = useAppStyle()
+  
   return (
     <>
       <AppBar position="static">
         <T variant="h2"><Icon color="red" /> Tarock Sheet</T>
       </AppBar>
-      <Container>
+      <Container className={classes.container}>
         <TarockSheet />
       </Container>
     </>
